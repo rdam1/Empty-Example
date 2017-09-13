35f699fb3c6c21898f1a54aa892830a45672a5a1
rdam1 <31661538+rdam1@users.noreply.github.com>
2017-09-13 11:43:55 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 600);
+}
+
+function draw() {
+  background (50, 100);
+  fill(63, 145, 227);
+  ellipse(mouseX, mouseY, 50, 50);
+  console.log("check, check, ");
+}
